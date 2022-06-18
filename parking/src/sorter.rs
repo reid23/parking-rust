e238280a4aba8ffa78067676158c35467c47415b
ReidDye <40401995+reid23@users.noreply.github.com>
2022-06-18 15:15:04 -0500

    writing index and get_assignments

--- parking/src/sorter.rs
+++ parking/src/sorter.rs
@@ -1,3 +1,5 @@
+use Student;
+
 pub struct Sorter{
     mut students: Vec<Vec<Student>>
 }
@@ -20,4 +22,41 @@ impl Sorter{
         for day in self.students{
             day.sort_by(|a| a.generate_score())
         }
+    }
+
+    fn index(&self, day: usize, student: Student) -> usize{
+        for student in self.students[day]{
+            match student{
+                
+            }
+        }
+    }
+
+    pub fn get_assignments(&self) -> Vec<Vec<String>>{
+        //should output [name, mon, tues, wed, thur, fri] for each student
+        //[name    mon    tue    wed    thu    fri]
+        //                ...
+        //[abcd    brt    reg    sml    sml    brt]
+
+
+        //*so start by doing this, then transpose
+        //*this way we can go day by day
+        //[stu1    stu2    stu3    stu4    ....    stuN]
+        //[zMon    zMon    zMon    zMon    ....    zMon]
+        //[zTue    zTue    zTue    zTue    ....    zTue]
+
+        let mut results: Vec<Vec<String>> = Vec::new();
+
+        for day in self.students{
+            let mut this_day_results: Vec<String> = vec![String::new(); day.len()];
+            for student in day{
+                if student.can_parallel_park(){
+                    results.
+                }
+            }
+            results.push(this_day_results);
+        }
+
+        //then transpose and return
+
     }
