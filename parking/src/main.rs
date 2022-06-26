ef3ef5bba04c8b1a5bac0749b89064646db40b3b
ReidDye <40401995+reid23@users.noreply.github.com>
2022-06-26 10:53:41 -0500

    added help messages

--- parking/src/main.rs
+++ parking/src/main.rs
@@ -8,12 +8,58 @@ use std::env::current_exe;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len()!=1{
-        //do stuff with setting and getting things
+        if args[1].contains("-h"){
+            main_help();
+            return;
+        }
+        if args[1] == "config" {
+            if args[2].contains("-h"){
+                config_help();
+                return;
+            }
+        }
+        if args[1] == "assign" {
+            if args[2].contains("-h"){
+                assign_help();
+                return;
+            }
+        }
     }
     else{
 
     }
     test();
+}
+fn assign_help(){
+    println!("usage: parking assign [-h] [-t TIME]\n");
+    println!("run the algorithm and assign parking for the week.\n");
+    println!("OPTIONS:");
+    println!("  -h, --help           show this help message and exit");
+    println!("  -t, --time [TIME]    assign parking at TIME today, where TIME is a 24 hour time given in HH:MM format (ie, -t 24:00 would mean midnight)");
+}
+fn config_help(){
+    println!("usage: parking config [-h] [-s] [-e] SETTING NEW_VALUE\n");
+    println!("change or view the current configuration.\n");
+    println!("POSITIONAL ARGUMENTS:");
+    println!("  SETTING         (optional) the setting to change.");
+    println!("  NEW_VALUE       (optional) the setting's new value. If given, SETTING must also be given.\n");
+    println!("OPTIONS:");
+    println!("  -h, --help      show this help message and exit");
+    println!("  -s, --show      show the given setting's value. If no setting is given, list all settings and their values.");
+    println!("  -e, --editor    open an editor with the setting to edit the value.")
+
+}
+
+fn main_help(){
+    println!("usage: parking [-h] [-v] <command> [<args>]\n");
+    println!("an app to assign on-campus parking spots.\n");
+    println!("OPTIONS:");
+    println!("   -h, --help      show this help message and exit");
+    println!("   -v, --version   print the current version and exit\n");
+    println!("COMMANDS:\n");
+    println!("  config           view or change the configuration of max sizes, weights, or paths.");
+    println!("  assign           runs the algorithm and assigns parking for the week\n");
+    println!("for help relating to config or assign, pass the -h flag: `~$ parking config -h`");
 }
 fn test(){
     let path_buffer = current_exe().unwrap();
@@ -22,8 +68,6 @@ fn test(){
     path_vec.pop();
     let mut path = path_vec.join("/");
     path.push_str("/.config.json");
-
-    
 
     let s = Sorter::Sorter::new(vec![
         vec![Student::Student::new("bob", &vec![1,94303,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], &path).unwrap(),
